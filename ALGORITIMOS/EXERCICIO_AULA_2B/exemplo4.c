51490fe6956c01b2c65a39bc89819d6465b24307
JeffersonDIG <jefferson8841@gmail.com>
2020-03-29 10:12:02 -0300

    Correção de diretorio

--- ALGORITIMOS/EXERCICIO_AULA_2B/exemplo4.c
+++ ALGORITIMOS/EXERCICIO_AULA_2B/exemplo4.c
@@ -3,25 +3,23 @@
 int main(int arg, char *args[])
 {
     int a;
-    int b;
+    char b;
     float c;
 
-    printf(": \n\n");
-
-    printf("informe a primeira nota:   \n");
-    scanf("%d", &np1);
-
-    printf("informe a segunda nota:   \n");
-    scanf("%d", &np2);
-
-    printf("informe a nota do trabalho:   \n");
-    scanf("%f", &pim);
-
-    ms = (np1 * 4 + pim * 2 + np2 * 4) / 10;
+    printf("\n\nDigite um valor inteiro para a:");
+    scanf("%i",&a);
+    printf("\n\nDigite um valor char para b:");
+    scanf("%s",&b);
+    printf("\n\nDigite um valor float para c:");
+    scanf("%f",&c);
 
     printf("\n\n");
-    printf("A sua media é:", ms);
-
+    printf("Valor da vairiavel a: %d\n",a);
+    printf("Posição da memoria de a: %p\n",&a);
+    printf("Valor da vairiavel b: %s\n",b);
+    printf("Posição da memoria de b: %p\n",&b);
+    printf("Valor da vairiavel c: %f\n",c);
+    printf("Posição da memoria de c: %p\n",&c);
     getch();
 
     return 0;
