87d120e5abc46d7d6c2661cb8163763eb930a2b3
JeffersonDIG <jefferson8841@gmail.com>
2020-05-06 21:55:20 -0300

    cor

--- ESTRUTURA_DE_DADOS/06-05-2020/exercicio.c
+++ ESTRUTURA_DE_DADOS/06-05-2020/exercicio.c
@@ -5,23 +5,31 @@ int main(int argc, char const *argv[])
 {
     nodetree *arvore;
     int num, cont;
-
+	cont = 1;
     do
     {
         printf("\nEntre com um numero inteiro:");
-        scanf("%d",&num);
-        if(cont == 1){
-             arvore = maketree(num);
-             cont++;
-        }else{
-        insert_binary_tree(arvore,num);
+        scanf("%d", &num);
+        if (num != 0)
+        {
+            if (cont == 1)
+            {
+                arvore = maketree(num);
+                cont++;
+            }
+            else
+            {
+                insert_binary_tree(arvore, num);
+            }
         }
     } while (!num == 0);
 
-printf("\n\n ordem\n"); percorrer_ordem(arvore);
-printf("\n\n ordem\n"); percorrer_preordem(arvore);
-printf("\n\n ordem\n"); percorrer_posordem(arvore);
-
+    printf("\n\n ordem\n");
+    percorrer_ordem(arvore);
+    printf("\n\n ordem\n");
+    percorrer_preordem(arvore);
+    printf("\n\n ordem\n");
+    percorrer_posordem(arvore);
 
     return 0;
 }
